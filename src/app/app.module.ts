3a2802b7993ca20c39f52ef291c961fbe45d31bf
MbonuJennifer <jennifer.mbonu@ietech.com.ng>
2023-10-12 14:02:26 +0100

    Project Pages Setup with Netlify Integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,29 @@ import { AppComponent } from './app.component';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { ContactComponent } from './Pages/contact/contact.component';
+import { PageNotFoundComponent } from './Pages/page-not-found/page-not-found.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { AllBooksComponent } from './components/all-books/all-books.component';
+import { FeaturedBooksComponent } from './components/featured-books/featured-books.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SigninComponent,
     SignupComponent,
-    SidebarComponent
+    SidebarComponent,
+    NavbarComponent,
+    HomeComponent,
+    ContactComponent,
+    PageNotFoundComponent,
+    HeroComponent,
+    AllBooksComponent,
+    FeaturedBooksComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
