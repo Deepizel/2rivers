ae60376151599e56e3a4fcd42ab1d2c6ebe67895
victor Ekundayo <victor.ekundayo@ietech.com.ng>
2023-10-13 19:51:50 +0100

    worked on orderlist page and dashboard cards

--- src/app/Pages/booklist/booklist.component.ts
+++ src/app/Pages/booklist/booklist.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-booklist',
   templateUrl: './booklist.component.html',
-  styleUrls: ['./booklist.component.css']
+  styleUrls: ['./booklist.component.css'],
 })
-export class BooklistComponent {
+export class BooklistComponent implements OnInit {
 
+  addBookForm!: FormGroup;
+  constructor(private fb: FormBuilder) {
+
+
+    this.addBookForm = this.fb.group({
+      id: 0,
+      firstName: [''],
+      lastName: [''],
+      email: [''],
+      phoneNumber: [''],
+      isAdmin: [false],
+      isSuperAdmin: [false],
+      password: [''],
+    });
+  }
+
+  ngOnInit(): void {
+
+  }
 }
